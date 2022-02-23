a038821bd28ee60288978b9fd331924fcb3edaef
emeduke <57918035+emeduke@users.noreply.github.com>
2022-02-23 17:32:07 +0100

    firstcommit

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,9 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHome(): {} {
+    return {title: 'Home Page'};
+    }
+  getAboutUs(): {} {
+    return {title: 'About Us Page'}; }
 }
