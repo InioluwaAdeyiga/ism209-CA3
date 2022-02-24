3a3f3619e22d2d742ab3559f03366f472e6f9f0b
emeduke <57918035+emeduke@users.noreply.github.com>
2022-02-24 21:34:13 +0100

    COMMIT5

--- src/citizen-registration/linkedidentity/linkedidentity.module.ts
+++ src/citizen-registration/linkedidentity/linkedidentity.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LinkedidentityService } from './linkedidentity.service';
 import { LinkedidentityController } from './linkedidentity.controller';
+import { linkedidentity } from './entities/linkedidentity.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Biodatum } from '../biodata/entities/biodatum.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([linkedidentity, Biodatum])],
   controllers: [LinkedidentityController],
   providers: [LinkedidentityService]
+  
 })
 export class LinkedidentityModule {}
